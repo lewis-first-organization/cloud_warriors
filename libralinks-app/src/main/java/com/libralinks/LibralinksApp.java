721da093c72811e6d66c73c237d51ece1a5f0181
Ubuntu <ubuntu@ip-172-31-45-77.ec2.internal>
2025-08-24 01:16:46 +0000

    my first commit 08-23-25

---
+++ libralinks-app/src/main/java/com/libralinks/LibralinksApp.java
@@ -0,0 +1,11 @@
+package com.libralinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibralinksApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LibralinksApp.class, args);
+    }
+}
